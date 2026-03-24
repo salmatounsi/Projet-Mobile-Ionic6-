962eccca3eaf5f25e0e7284d1e170bbb310bfa20
Nour Bsila <nour.bsila@gmail.com>
2026-03-24 13:12:04 +0100

    fixed auth-service error

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit ,inject} from '@angular/core';
-import {AuthService} from "../../services/auth-service";
 import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth-service";
 
 @Component({
   selector: 'app-login',
